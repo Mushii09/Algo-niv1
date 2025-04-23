0084b75b9968aa943d7d9de841cbb8292cbd1d32
Mushii09 <benjamin.fabnum5@gmail.com>
2025-04-23 08:50:45 +0200

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+  /***--------Donner un nom à votre class -------------------****/
+
+  public class HelloWorld {
+
+    public static void main(String[] args) {
+
+          /***--------  Votre code ici  -------------------****/
+    System.out.println ("Hello World");
+
+
+    }
+}
